ecbcf7a007a3b53562d7fd54c3f643a4d368b685
Full Stack <fullstack@fullstacks-MacBook-Pro.local>
2020-02-02 12:01:20 -0500

    update js code and running the calendar

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # week-05-work-day-scheduler
-Week 05 Work Day Scheduler
+## Week 05 Work Day Scheduler - UCF Coding BootCamp
+
+- Components in this repo:
+  - HTML
+  - CSS
+  - Javascript
+  - README
+  
+  This code was made it in JavaScript to create a Work Day Scheduler App with moment.js.
+
+
+Check my work  - Click!
+[HERE](https://fullstackforce.github.io/week-05-work-day-scheduler/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,39 @@ $( document ).ready(function() {
         row.append(col2);
         row.append(col3);
 
-        // adding rows to container
+        // add rows to container
         $(".container").append(row);
 
         getLocalStorage(i);
     }
+        
+    function formatAMPM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+formatAMPM();
+
+function updateColors(){
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) { 
+        console.log(currentTime, $(`#${i}`).data("time"));
+         if ($(`#${i}`).data("time") == currentTime){
+            $(`#text${i}`).addClass( "present");
+        } else if (currentTime < $(`#${i}`).data("time")) {
+            $(`#text${i}`).addClass( "future");
+        }
+    }
+}
+
+setInterval(function() {
+    updateColors();
+}, 1000);
+
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.description').val();
+    localStorage.setItem(eventId, eventText);
+});});
